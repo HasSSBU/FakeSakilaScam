178abce42292779b8ad72634d28fbb1bc8315a92
Harry <harry.johnson5481@gmail.com>
2022-10-20 11:55:18 +0100

    Initialising repo

---
+++ src/main/java/SakilaScam/SakilaScamApplication.java
@@ -0,0 +1,13 @@
+package SakilaScam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaScamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaScamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SakilaScam/SakilaScamApplicationTests.java
@@ -0,0 +1,13 @@
+package SakilaScam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class SakilaScamApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+
+//}
