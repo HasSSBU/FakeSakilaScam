53fe80ae56f93c5ac75599a49d07babec7ef51b0
Harry <harry.johnson5481@gmail.com>
2022-10-20 12:33:05 +0100

    setting up sonarcloud analysis

--- src/main/java/SakilaScam/SakilaScamApplication.java
+++ src/main/java/SakilaScam/SakilaScamApplication.java
@@ -1,9 +1,9 @@
 package SakilaScam;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SakilaScamApplication {
 
 	public static void main(String[] args) {
