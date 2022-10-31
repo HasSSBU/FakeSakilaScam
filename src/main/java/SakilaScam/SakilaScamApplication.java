3615256db8b57c8a0cbc6ee83e0710b19d57d8c7
Harry <harry.johnson5481@gmail.com>
2022-10-31 10:06:52 +0000

    uncommented Put Actors and Delete Actors (has error)

--- src/main/java/SakilaScam/SakilaScamApplication.java
+++ src/main/java/SakilaScam/SakilaScamApplication.java
@@ -35,30 +35,30 @@ public class SakilaScamApplication {
 		return actorRepository.findAll();
 	}
 
-//	@PutMapping("/putActors/{id}")
-//	public ResponseEntity<Actor> updateActor(@PathVariable(value="id") int actor_id,
-//											 @Validated @RequestBody Actor actorDetails) throws ResourceAccessException {
-//		Actor actor = actorRepository.findById(actor_id)
-//				.orElseThrow(() -> new ResourceAccessException("Actor ID not found : : " + actor_id));
-//
-//
-//		actorDetails.setActor_id(actorDetails.getActor_id());
-//		actorDetails.setFirstName(actorDetails.getFirstName());
-//		actorDetails.setLastName(actorDetails.getLastName());
-//		final Actor updatedActor = actorRepository.save(actorDetails);
-//		return ResponseEntity.ok(updatedActor);
-//	}
-//
-//	@DeleteMapping("/Delete/{id}")
-//	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") int actor_id)
-//			throws ResourceAccessException {
-//		Actor actorHere = actorRepository.findById(actor_id)
-//				.orElseThrow(() -> new ResourceAccessException("Employee not found for this id :: " + actor_id));
-//
-//		actorRepository.delete(actorHere);
-//		Map<String, Boolean> response = new HashMap<>();
-//		response.put("deleted", Boolean.TRUE);
-//		return response;
-//	}
+	@PutMapping("/putActors/{id}")
+	public ResponseEntity<Actor> updateActor(@PathVariable(value="id") int actor_id,
+											 @Validated @RequestBody Actor actorDetails) throws ResourceAccessException {
+		Actor actor = actorRepository.findById(actor_id)
+				.orElseThrow(() -> new ResourceAccessException("Actor ID not found : : " + actor_id));
+
+
+		actorDetails.setActor_id(actorDetails.getActor_id());
+		actorDetails.setFirstName(actorDetails.getFirstName());
+		actorDetails.setLastName(actorDetails.getLastName());
+		final Actor updatedActor = actorRepository.save(actorDetails);
+		return ResponseEntity.ok(updatedActor);
+	}
+
+	@DeleteMapping("/Delete/{id}")
+	public Map<String, Boolean> deleteEmployee(@PathVariable(value = "id") int actor_id)
+			throws ResourceAccessException {
+		Actor actorHere = actorRepository.findById(actor_id)
+				.orElseThrow(() -> new ResourceAccessException("Employee not found for this id :: " + actor_id));
+
+		actorRepository.delete(actorHere);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+	}
 
 }
